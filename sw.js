3d2bd1ed68d584e2ca024f1ce46038f82d47e7fe
cww-soucy <cwweil@soucyaquatik.com>
2026-08-25 08:08:59 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SA Platform — Service Worker
-// NOTIFICATIONS DÉSACTIVÉES. Force la mise à jour du cache (v31).
+// NOTIFICATIONS DÉSACTIVÉES. Force la mise à jour du cache (v33).
 
-var CACHE_NAME = 'sa-platform-v31';
+var CACHE_NAME = 'sa-platform-v33';
 
 // Installation : s'activer tout de suite sans attendre
 self.addEventListener('install', function(event) {
